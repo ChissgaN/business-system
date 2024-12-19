353c70ab44df05e67812b30d7fbe938cf77645e0
ChissgaN <coderchissgan@gmail.com>
2024-12-19 12:18:49 -0600

    added triggers after update and delete  product_sales and update  purchase_products

--- database/triggers.sql
+++ database/triggers.sql
@@ -27,7 +27,7 @@ AFTER UPDATE ON purchase_products
 FOR EACH ROW
 BEGIN
     DECLARE current_stock INT;
-    DECLARE new_stock INT;
+    DECLARE quantity_difference INT;
 
     IF NEW.received = 1 THEN
         SELECT qty INTO current_stock 
@@ -35,13 +35,16 @@ BEGIN
         WHERE id = NEW.product_id 
         FOR UPDATE;
 
+        SET quantity_difference = NEW.qty - OLD.qty;
+
         UPDATE products 
-        SET qty = (current_stock + NEW.qty) 
+        SET qty = (current_stock + quantity_difference) 
         WHERE id = NEW.product_id;
     END IF;
 END;
 //
 DELIMITER ;
+
 
 DROP TRIGGER IF EXISTS after_product_sales_insert;
 DELIMITER //
@@ -70,16 +73,39 @@ AFTER UPDATE ON product_sales
 FOR EACH ROW
 BEGIN
     DECLARE current_stock INT;
-    DECLARE new_stock INT;
+    DECLARE quantity_difference INT;
 
     SELECT qty INTO current_stock 
     FROM products 
     WHERE id = NEW.product_id 
     FOR UPDATE;
 
+    SET quantity_difference = OLD.qty - NEW.qty;
+
     UPDATE products 
-    SET qty = (current_stock - NEW.qty) 
+    SET qty = (current_stock + quantity_difference) 
     WHERE id = NEW.product_id;
 END;
 //
 DELIMITER ;
+
+DROP TRIGGER IF EXISTS after_product_sales_delete;
+DELIMITER //
+CREATE TRIGGER after_product_sales_delete
+AFTER DELETE ON product_sales
+FOR EACH ROW
+BEGIN
+    DECLARE current_stock INT;
+
+    SELECT qty INTO current_stock 
+    FROM products 
+    WHERE id = OLD.product_id 
+    FOR UPDATE;
+
+    UPDATE products 
+    SET qty = (current_stock + OLD.qty) 
+    WHERE id = OLD.product_id;
+END;
+//
+DELIMITER ;
+
